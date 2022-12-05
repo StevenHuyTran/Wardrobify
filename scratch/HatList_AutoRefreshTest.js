8b3ed302bbaa85b81ed188b121adcaf20f151fa1
Steven <Stevenhuytran03@gmail.com>
2022-12-05 08:55:15 -0800

    Revised HatList.js

--- scratch/HatList_AutoRefreshTest.js
+++
@@ -1,9 +0,0 @@
-const deleteHat = async () => {
-  fetch(`http://localhost:8090/api/hats/${currentHat.id}/`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  window.location.reload();
-};
