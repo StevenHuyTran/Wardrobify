9a2fb396b314aad10225e7f88146c355dc30ef7d
Steven <Stevenhuytran03@gmail.com>
2022-12-05 16:37:14 -0800

    deleted scratch files

--- ghi/app/src/HatsList.js
+++ ghi/app/src/HatsList.js
@@ -2,7 +2,7 @@ import React, { useState, setItems } from "react";
 import { Link } from "react-router-dom";
 
 function HatsList(props) {
-  const [items, setItems] = React.useState(props.hats);
+  const [items, setItems] = (React.useState(props.hats));
   const deleteItem = (id) => async () => {
     const url = `http://localhost:8090/api/hats/${id}/`;
     const fetchConfig = {
